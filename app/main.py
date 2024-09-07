59a3504bfd65f0961b17c4baed7b985970e6027c
ravishankardas <ravishankerdas020@gmail.com>
2024-09-07 21:02:53 +0530

    pushing changes to heroku?

--- app/main.py
+++ app/main.py
@@ -27,4 +27,4 @@ app.include_router(vote.router)
 
 @app.get("/")
 def root():
-    return {"message": "Hello World, i am back"}
+    return {"message": "Hello World, i have deployed this app on heroku"}
